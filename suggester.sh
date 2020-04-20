dd9177607a4a3ee14234779fe73d33fc6bd0d3ba
Weirdatfirst <weirdatfirst@gmail.com>
2020-04-20 08:40:13 -0400

    updates

--- suggester.sh
+++ suggester.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# scriptdir="/root/tools/hackthebox"
-# ip_address="10.10.10.184"
-# basedir="/root/ctf/htb/"
-# test_name="servmon"
-
-
 
 outdir=${basedir}${test_name}
 declare -a common_ports
-common_ports=(80 139 443 445)
+common_ports=(80 139  389 443 445)
 inputfile=${basedir}${test_name}/nmap/${test_name}_full.nmap
 echo $inputfile
 for i in "${common_ports[@]}"
@@ -20,12 +14,16 @@ do
 	elif [[ $isopen == 139/tcp ]]; then
 		tmux new-window -d -n smb
 		tmux send-keys -t smb "smbclient -L ${ip_address} -N" Enter
-	elif [[ $isopen == 445/tcp ]]; then
-		tmux new-window -d -n smb2
-		tmux send-keys -t smb2 "smbclient -L ${ip_address} -N" Enter
+	elif [[ $isopen == 389/tcp  ||  $isopen == 389/tcp ]]; then
+		tmux new-window -d -n ldap
+		tmux send-keys -t ldap "ldapsearch -x -h "${ip_address}" -s base namingcontexts" Enter
 	elif [[ $isopen == 443/tcp  ||  $isopen == 443/tcp ]]; then
 		tmux new-window -d -n dirbs
 		tmux send-keys -t dirbs "bash ${scriptdir}dirb_https.sh ${ip_address} ${outdir}" Enter
+	elif [[ $isopen == 445/tcp ]]; then
+		tmux new-window -d -n smb2
+		tmux send-keys -t smb2 "smbclient -L ${ip_address} -N" Enter
+
 	else 
 		continue
 	fi
