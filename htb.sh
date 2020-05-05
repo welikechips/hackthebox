393a31f6c04c3c192f7e5486dee4808a37911eec
tnory56 <tnorberg@thomasnorberg.com>
2020-05-05 09:59:31 -0400

    Updated version

--- htb.sh
+++ htb.sh
@@ -2,16 +2,41 @@
 # Description: Creates folders and setups tmux
 ### Research launching from inside another tmux
 scriptdir="/root/tools/hackthebox"
-basedir="/root/ctf/htb/"
-mkdir -p $basedir
-read -r -p "What box are you working on?" test_name
-read -r -p "What is the IP address?" ip_address
+basedir="/root/ctf/htb"
+read -r -p "What box are you working on? " test_name
+read -r -p "What is the IP address? " ip_address
 
-mkdir -p $basedir/$test_name/
-mkdir -p $basedir/$test_name/nmap
-mkdir -p $basedir/$test_name/burp
-mkdir -p $basedir/$test_name/dirb
-cd $basedir/$test_name
+WD=$basedir/$test_name/
+mkdir -p $WD
+cd $WD
+mkdir -p nmap
+mkdir -p burp
+mkdir -p dirb
+
+export IP=${ip_address}
+export WD=${WD}
+
+server_name_found=$(grep -F "${ip_address} " /etc/hosts | awk '{ print $2 }')
+
+if [ ! -z "$server_name_found" ]
+then
+    echo "${server_name_found} found in the /etc/hosts file with the ip address: ${ip_address}"
+fi
+
+read -r -p "Update hosts file? [y/N] " response
+
+server_name=${test_name}.htb
+case "$response" in
+    [yY][eE][sS]|[yY])
+        sudo echo -e "${IP}\t${server_name}" >> /etc/hosts
+        echo "Current IP Address is ${IP}, server name is ${server_name} and current working directory is ${WD}"
+        ;;
+    *)
+        echo "Current IP Address is ${IP} and current working directory is ${WD}"
+	;;
+esac
+
+read -p "Press [Enter] key to start the process..."
 
 #launchs new session with box name
 tmux new-session -d -s $test_name
@@ -20,3 +45,5 @@ tmux send-keys -t "$test_name:nmap" "bash $scriptdir/nmap.sh $test_name $ip_addr
 tmux send-keys -t "$test_name" "openvpn /root/ctf/htb/client.ovpn" Enter
 #Passess the variables to the nmap script to start scanning
 tmux attach -t $test_name
+tmux setenv IP ${ip_address}
+tmux setenv WD ${WD}
