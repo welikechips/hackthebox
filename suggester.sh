5829498e7849d466ed5b20197199a68b09e990d3
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2022-02-23 11:58:47 -0500

    Update suggester.sh

--- suggester.sh
+++ suggester.sh
@@ -4,6 +4,7 @@ declare -a common_ports
 common_ports=(80 139  389 443 445)
 inputfile=${basedir}${test_name}/nmap/${test_name}_full.nmap
 echo $inputfile
+wordlist="/usr/share/wordlists/SecLists/Discovery/DNS/shubs-subdomains.txt"
 for i in "${common_ports[@]}"
 do
 	isopen=`(cat $inputfile | grep -w ${i}/tcp | cut -d " " -f1)`
@@ -11,7 +12,9 @@ do
 		tmux new-window -d -n dirb
 		tmux send-keys -t dirb "bash ${scriptdir}/dirb_http.sh ${ip_address} ${outdir}" Enter
 		tmux new-window -d -n ffuf80
-		tmux send-keys -t ffuf80 "ffuf -u http://${test_name}.htb -H \"Host: FUZZ.${test_name}.htb\" -w /usr/share/wordlists/SecLists/Discovery/DNS/shubs-subdomains.txt -mc 200" Enter
+		tmux send-keys -t ffuf80 "ffuf -u http://${test_name}.htb -H \"Host: FUZZ.${test_name}.htb\" -w ${wordlist} -mc 200" Enter
+		tmux split-window -t ffuf80 -v
+		tmux send-keys -t ffuf80 "ffuf -u http://${test_name} -H \"Host: FUZZ.${test_name}\" -w ${wordlist} -mc 200" Enter
 	elif [[ $isopen == 139/tcp ]]; then
 		tmux new-window -d -n smb
 		tmux send-keys -t smb "smbclient -L ${ip_address} -N" Enter
@@ -22,7 +25,9 @@ do
 		tmux new-window -d -n dirbs
 		tmux send-keys -t dirbs "bash ${scriptdir}/dirb_https.sh ${ip_address} ${outdir}" Enter
 		tmux new-window -d -n ffuf443
-		tmux send-keys -t ffuf443 "ffuf -u https://${test_name}.htb -H \"Host: FUZZ.${test_name}.htb\" -w /usr/share/wordlists/SecLists/Discovery/DNS/shubs-subdomains.txt -mc 200" Enter
+		tmux send-keys -t ffuf443 "ffuf -u https://${test_name}.htb -H \"Host: FUZZ.${test_name}.htb\" -w ${wordlist} -mc 200" Enter
+		tmux split-window -t ffuf80 -v
+		tmux send-keys -t ffuf443 "ffuf -u https://${test_name} -H \"Host: FUZZ.${test_name}\" -w ${wordlist} -mc 200" Enter
 	elif [[ $isopen == 445/tcp ]]; then
 		tmux new-window -d -n smb2
 		tmux send-keys -t smb2 "smbclient -L ${ip_address} -N" Enter
