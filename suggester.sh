04546c679eb937a361d6c005ed53b25399a4c1aa
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2023-03-21 16:21:02 -0400

    Update suggester.sh

--- suggester.sh
+++ suggester.sh
@@ -8,13 +8,20 @@ wordlist="/usr/share/wordlists/SecLists/Discovery/DNS/shubs-subdomains.txt"
 for i in "${common_ports[@]}"
 do
 	isopen=`(cat $inputfile | grep -w ${i}/tcp | cut -d " " -f1)`
-	if [[ $isopen == 80/tcp || $isopen == 8080/tcp ]] ;then
+	if [[ $isopen == 80/tcp ]] ;then
 		tmux new-window -d -n dirb
 		tmux send-keys -t dirb "bash ${scriptdir}/dirb_http.sh ${test_name}.htb ${outdir}" Enter
 		tmux new-window -d -n ffuf80
 		tmux send-keys -t ffuf80 "bash ${scriptdir}/ffuf.sh http://${test_name}.htb FUZZ.${test_name}.htb ${wordlist}" Enter
 		tmux split-window -t ffuf80 -v
 		tmux send-keys -t ffuf80 "bash ${scriptdir}/ffuf.sh http://${test_name} FUZZ.${test_name} ${wordlist}" Enter
+	elif [[ $isopen == 8080/tcp ]] ;then
+		tmux new-window -d -n dirb
+		tmux send-keys -t dirb "bash ${scriptdir}/dirb_http.sh ${test_name}.htb:8080 ${outdir}" Enter
+		tmux new-window -d -n ffuf8080
+		tmux send-keys -t ffuf8080 "bash ${scriptdir}/ffuf.sh http://${test_name}.htb:8080 FUZZ.${test_name}.htb ${wordlist}" Enter
+		tmux split-window -t ffuf8080 -v
+		tmux send-keys -t ffuf8080 "bash ${scriptdir}/ffuf.sh http://${test_name}:8080 FUZZ.${test_name} ${wordlist}" Enter
 	elif [[ $isopen == 139/tcp ]]; then
 		tmux new-window -d -n smb
 		tmux send-keys -t smb "smbclient -L ${ip_address} -N" Enter
