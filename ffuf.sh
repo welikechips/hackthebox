670f237f3ce888685f4b57292d17147b445df885
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2023-03-21 15:54:39 -0400

    Create ffuf.sh

---
+++ ffuf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#fix - $command comment
+clear
+url=$1
+fuzz=$2
+wordlist=$3
+if [ -z "$1" ]
+then
+	read -r -p "What is the url?" url
+  read -r -p "What is the fuzz?" fuzz
+  read -r -p "What is the wordlist?" wordlist  
+	command="ffuf -u \"${url}\" -H \"Host: ${fuzz}\" -w ${wordlist} -mc 200""
+	echo $command
+	echo $command | xclip -sel clip
+else
+	command="ffuf -u \"${url}\" -H \"Host: ${fuzz}\" -w ${wordlist} -mc 200""
+	echo $command
+	$command
+fi
