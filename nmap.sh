c0257f68483986f7921d52a25c3fa08e46ea5ff2
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2023-05-03 10:19:40 -0400

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -9,8 +9,12 @@ clear
 read -r -p "Do you want to run NMAP? [y/N] " nmapresponse
 case "$nmapresponse" in
     [yY][eE][sS]|[yY])
-		#Starts nmaping all ports on box
-        nmap -sC -sV $ip_address -oA ${basedir}${test_name}/nmap/${test_name}_full -p-
+	#Starts nmaping all ports on box
+	workfile=${basedir}${test_name}/nmap/${test_name}_full
+	rm /root/nmap-bootstrap.xsl
+	wget https://j1v37u2k3y.github.io/assets/reports/nmap/nmap-bootstrap.xsl -O /root/nmap-bootstrap.xsl
+        nmap -sC -sV $ip_address -oA ${workfile} -p- --stylesheet /root/nmap-bootstrap.xsl
+	xsltproc -o ${workfile}.html /root/nmap-bootstrap.xsl ${workfile}.xml
         ;;
     *)
         
