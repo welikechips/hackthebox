fda7d28787ac303ffc9764c494e3cafecb0c5508
weirdatfirst <30903493+weirdatfirst@users.noreply.github.com>
2024-02-02 15:19:27 -0500

    Update dirb_http.sh
    Moved to gobuster

--- dirb_http.sh
+++ dirb_http.sh
@@ -5,10 +5,10 @@ outdir=$2
 if [ -z "$1" ]
 then
 	read -r -p "What is the url?" url
-	command="dirsearch -u $url -w /usr/share/wordlists/dirb/big.txt -f -t 100 -r -e php,html,js,txt -x 403"
+	command="gobuster dir -u $url -w /usr/share/wordlists/dirb/big.txt -t 100 -r -x php,html,js,txt -k "
 	echo $command
 	echo $command | xclip -sel clip
 else
-	command="dirsearch -u http://${1} -w /usr/share/wordlists/dirb/big.txt -f -t 20 -r -e php,html,js,txt -x 403"
+	command="gobuster dir -u http://${1} -w /usr/share/wordlists/dirb/big.txt -t 20 -r -x php,html,js,txt -k "
 	$command
 fi
